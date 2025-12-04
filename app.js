1aad81abb06934c254259a79bef62c4061f5f0f2
Daniel Fernandes de Moraes <ti.danielmoraes@gmail.com>
2025-12-04 10:21:35 +0000

    Criada conta2

--- app.js
+++ app.js
@@ -6,3 +6,32 @@ class Conta {
         this.historico = [];
     }
 }
+
+const conta2 = new Conta(
+    nome = 'Daniel Moraes',
+    saldo = 1500,
+    senha = 'daniel123',
+)
+console.log(conta2);
+
+function levantarConta(nome, saldo, senha) {
+    return new Conta(nome, saldo, senha);
+}
+
+function depositar(conta, valor) {
+    if (valor <= 0) {
+        console.log("O valor do depósito deve ser positivo.");
+    }
+    conta.saldo += valor;
+    conta.historico.push({ tipo: 'depósito', valor: valor });
+}
+
+function consultarSaldo(conta, senha) {
+    if (senha !== conta.senha) {
+        console.log("Senha incorreta.");
+        return;
+    }   else {
+        console.log(`Saldo atual: R$ ${conta.saldo.toFixed(2)}`);
+    }
+}
+
