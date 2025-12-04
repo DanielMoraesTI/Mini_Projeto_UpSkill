3136cb4e4e8c88ce1b526febc231334c9e82a6a3
Taís Dias <taisayana@hotmail.com>
2025-12-04 10:53:10 +0000

    Integrando conta1 e conta3

---
+++ app.js
@@ -0,0 +1,23 @@
+// BankSimulator
+
+const conta1 = new Conta(
+  nome = "Taís Dias",
+  saldo = 1000.00,
+  senha = "1234"
+)
+console.log(conta1);
+
+
+const conta3 = new Conta(
+  nome = "Tereza Dias",
+  saldo = 10000.00,
+  senha = "1357"
+)
+console.log(conta3);
+
+
+
+
+
+
+
