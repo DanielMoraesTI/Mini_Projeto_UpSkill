a5357a7f2dbdc244c87419a601eb74672d4a3bc7
Daniel Fernandes de Moraes <ti.danielmoraes@gmail.com>
2025-12-05 10:22:01 +0000

    Acrescentada função para consulta de histórico Global e 2 funções extras

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ class Conta {
 const conta1 = new Conta("Taís Dias", 1000, "1234");
 const conta2 = new Conta('Daniel Moraes', 1500, '135');
 const conta3 = new Conta("Tereza Dias", 10000, "1357");
-const conta4 = new Conta('Elane Assis', 3000, '246');
+const conta4 = new Conta('Elane Assis', 4600, '246');
 
 // array de contas (penso que seria mais interessante criar uma função criarContas, que "criasse as novas contas" e fizesse um push neste array)
 const contas = [conta1, conta2, conta3, conta4];
+//array pata armazenar todas as oprerações realizadas
+const extratoGlobal = [];
 
 
 // Funções
@@ -30,6 +32,7 @@ function levantar(conta, valor, pin) {
     }  
     conta.saldo -= valor;
     conta.historico.push({tipo: "Levantamento", valor: valor.toFixed(2)});
+    extratoGlobal.push({tipo: "Levantamento", valor: valor.toFixed(2), nome: conta.nome});
 }
 
 
@@ -40,6 +43,7 @@ function depositar(conta, valor) {
     }
     conta.saldo += valor;
     conta.historico.push({tipo: "Depósito", valor: valor.toFixed(2) });
+    extratoGlobal.push({tipo: "Depósito", valor: valor.toFixed(2), nome: conta.nome});
     console.log(`Depósito no valor de ${valor.toFixed(2)} € efetuado com sucesso`);
 }
 
@@ -51,6 +55,7 @@ function consultarSaldo(conta, pin) {
     }   else {
         console.log(`Saldo atual: ${conta.saldo.toFixed(2)} €`);
         conta.historico.push({tipo: "Consultar saldo"});
+        extratoGlobal.push({tipo: "Consultar saldo", nome: conta.nome});
     }
 }
 
@@ -67,7 +72,9 @@ function transferencia(contaOrigem, contaDestino, valor, pin) {
     contaDestino.saldo += valor;
     console.log(`Transferência de ${contaOrigem.nome}, no valor de ${valor.toFixed(2)} € para ${contaDestino.nome} efetuada com sucesso.`);
     contaOrigem.historico.push({tipo: "Transfêrencia - enviada", valor: valor.toFixed(2)});
+    extratoGlobal.push({tipo: "Transfêrencia - enviada", valor: valor.toFixed(2), nome: contaOrigem.nome});
     contaDestino.historico.push({tipo: "Transfêrencia - recebida", valor: valor.toFixed(2)});
+    extratoGlobal.push({tipo: "Transfêrencia - recebida", valor: valor.toFixed(2), nome: contaDestino.nome});
 }
 
 
@@ -83,6 +90,7 @@ function transferencia(contaOrigem, contaDestino, valor, pin) {
     conta.pin = novoPin;
     console.log(conta.nome + ", seu PIN foi alterado com sucesso");
     conta.historico.push({tipo: "Alteração de senha"});
+    extratoGlobal.push({tipo: "Alteração de senha", nome: conta.nome});
  }
 
 
@@ -109,6 +117,17 @@ function clientesVip(arrayContas) {
     }
 }
 
+// Função para exibir o extrato global
+function exibirExtratoGlobal() {
+    for (let i = 0; i < extratoGlobal.length; i++) {
+        const operacao = extratoGlobal[i];
+        if (operacao.valor) {
+            console.log(`${operacao.nome} - ${operacao.tipo}: ${operacao.valor} €`);
+        } else {
+            console.log(`${operacao.nome} - ${operacao.tipo}`);
+        }
+    }
+}
 
 // Uma das operações adicionais
 function poupanca(conta, pin) {
@@ -123,11 +142,36 @@ function poupanca(conta, pin) {
     const percentual = 6.17;
     let rendimento = conta.saldo * (percentual / 100);
     console.log(`A sua poupança rendeu ${rendimento.toFixed(2)} €`);
-    conta.saldo -= rendimento;  // ERRO DE LÓGICA -> deveria ser +=
+    conta.saldo += rendimento;  
     conta.historico.push({tipo: "Poupança", valor: rendimento.toFixed(2)});
+    extratoGlobal.push({tipo: "Poupança", valor: rendimento.toFixed(2), nome: conta.nome});
 }
 
-// Este erro, faz com que o rendimento seja subtraído da conta, aos invés de acrescentado. 
+// Segunda operação adicional (avisar que chegou ao limite de consultas de saldo sem custos para o cliente)
+function quantidadeConsultaSaldo(conta) {
+    let contador = 0;
+    for (let i = 0; i < conta.historico.length; i++) {
+        if (conta.historico[i].tipo === "Consultar saldo") {
+            contador++;
+        }
+    }
+        if (contador = 3) {
+            console.log(`Atenção ${conta.nome}, você já consultou seu saldo ${contador} vezes. Atingiu o limite de consultas gratuitas neste mês. Será cobrada uma taxa de 2 € por cada consulta adicional.`);
+        } 
+    return contador;
+    }
+
+// Terceira operação adicional (avisar quanto falta para se tornar um cliente VIP)
+function avisoClienteVip(conta) {
+    const valorMinimoVip = 5000;
+    if (conta.saldo >= valorMinimoVip) {
+        console.log(`Parabéns ${conta.nome}, você é um cliente VIP!`);
+    } else {
+        console.log(`Olá ${conta.nome}, você não é um cliente VIP. Deposite mais ${ (valorMinimoVip - conta.saldo).toFixed(2)} € para se tornar um!`);
+    }
+}
+
+avisoClienteVip(conta1);
 
 
 function main () {
@@ -183,64 +227,7 @@ function main () {
     console.log(conta4);
 }
 
-main();
+//main();
 
 
-// Testes
-// Levantamento
-/*
-levantar(conta1, 100, "1134"); // pin incorreto
-levantar(conta2, 2000, "135"); // saldo insuficiente
-levantar(conta3, 150, "1357"); // levantamento efetuado com sucesso
-console.log(conta3);
-*/
-
-
-// Depósito
-/*
-depositar(conta2, 0);   // deposito com valor igual a 0
-depositar(conta4, 500); // depósito efetuado
-console.log(conta4);    // exibe a conta com o saldo atualizado
-*/
-
-
-// Consulta de Saldo
-/*
-consultarSaldo(conta2, "125");  // solicita o saldo, com PIN incorreto
-consultarSaldo(conta4, "246");  // PIN correto, exibe o saldo
-*/
-
-
-// Transferência
-/*
-transferencia(conta1, conta4, 250, "1234");  //faz a transferÊncia
-console.log(conta1);    // exibe as contas com os saldos atualizados
-console.log(conta4);
-*/
-
-
-// Alteração do PIN
-/*
- alterarPin(conta3, "1356", "1122"); // pin inválido
- alterarPin(conta1, "1234");         // novoPin vazio
- alterarPin(conta1, "1234", "0987"); // alteração efetuada
- console.log(conta1);                // exibe a conta com o pin alterado
-*/
-
-
-// Capital Total
-// capitalTotal(contas);
-
-
-// ClientesVIP
-/*
-clientesVip(contas);
-*/
-
-
-// Poupança
-/*
-poupanca(conta1, "1233");
-poupanca(conta2, "135");
-console.log(conta2);
-*/
+//exibirExtratoGlobal();
